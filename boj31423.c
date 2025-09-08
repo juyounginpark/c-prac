90ab52c946c875ccd6ea29229953aa9069733d5b
juyoung <0620ffff@naver.com>
2025-09-08 14:21:46 +0900

    20250908

--- boj31423.c
+++ boj31423.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct locate {
-	int order, locate;
-}arr[500001];
-int main() {
+typedef struct {
+	int len, locate;
+}M;
+
+M MAP[500001];
+
+char str[500001];
+char buffer[500001];
+
+void swap(M *a, M *b) {
+	M temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void printA(int n) {
+	puts("");
+	for(int i=0; i<n; i++) {
+        for(int l=0; l<MAP[i].len;l++) {
+            putchar(str[MAP[i].locate + l]);
+        }
+        putchar(' ');
+    }
+	puts("");
+}
+void main() {
 	int n;
 	scanf("%d", &n);
 	
-	char str[500001];
-	char buffer[500001];
-	int len = 0;
-	for(int i = 0; i < n; i++) {
-		scanf("%d", buffer);
+	int sum = 0;
+	for(int i=0; i<n; i++) {
+		scanf("%s", buffer);
+		MAP[i].len = strlen(buffer);
+		MAP[i].locate = sum;
+		sum += MAP[i].len;
 		strcat(str, buffer);
-		len += strlen(buffer) - 1;
-		arr[i].order = len;
-		
-
 	}
-
-
+	int a, b;
+	for(int i=0; i<n-1; i++) {
+		scanf("%d %d", &a, &b);
+		swap(&MAP[a-1], &MAP[b-1]);
+		printA(n);	
+	
+	}
+		
 }
