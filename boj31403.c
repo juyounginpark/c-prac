9280a32e616eef620813196d5690fe7e41a5af8d
juyoung <0620ffff@naver.com>
2025-09-26 09:27:33 +0900

    20250926:2

--- boj31403.c
+++ boj31403.c
@@ -7,8 +7,8 @@ int main() {
 
 	scanf("%s\n%s\n%s", a, b, c);
 
-	int A = stoi(A) + stoi(B) + stoi(C);
+	int A = atoi(a) + atoi(b) - atoi(c);
 	strcat(a, b);
-	int B = stoi(a) - stoi(b);
+	int B = atoi(a) - atoi(c);
 	printf("%d\n%d\n", A, B);
 }
